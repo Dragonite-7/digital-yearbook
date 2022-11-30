70070a7c0d099ca02b09ded550b1f42b6f265457
Kyle McCracken <kmccracken150@gmail.com>
2022-11-30 13:08:18 -0500

    prepare next/react to use GQL client-side

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,19 @@
-import '../styles/globals.css'
-import { SessionProvider } from 'next-auth/react'
+import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import React from 'react';
 
-export default function App({ Component, pageProps, session }) {
+export default function App({ Component, pageProps, session }: any) {
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    // uri: 'http://localhost:4000/graphql',
-    uri:'https://api.spacex.land/graphql/'
-    
+    uri: 'http://localhost:3000/api/graphql',
   });
   return (
     <SessionProvider session={session}>
       <ApolloProvider client={client}>
-        <Component {...pageProps}/>
+        <Component {...pageProps} />
       </ApolloProvider>
     </SessionProvider>
-  )
+  );
 }
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,11 @@
 import React from 'react';
 import styles from '../styles/Home.module.css';
 import HomePage from './lading-page';
-import {client} from '../server/apollo-client'
-import { gql } from '@apollo/client';
 
-export default function Home({countries}) {
-  console.log('countries-->', countries)
+export default function Home() {
   return (
     <div className={styles.container}>
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
-
-export async function getStaticProps() {
-  const { data} = await client.query({
-    query: gql`
-      query Countries {
-        countries {
-          code
-          name
-          emoji
-        }
-      }
-    `,
-  });
-  return {
-    props: {
-      countries: data.countries.slice(0,4)
-    }
-  }
-}
-
