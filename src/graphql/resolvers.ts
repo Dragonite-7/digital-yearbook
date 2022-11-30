d99498a0161fb3b976be637a2bced9838e0e3053
Kyle McCracken <kmccracken150@gmail.com>
2022-11-30 13:08:39 -0500

    update types and resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,27 +1,13 @@
-
-
-const getUsers =  require('../server/controllers/user.controllers');
-const pool = require('../server/data/db');
-import { v4 as uuidv4 } from 'uuid';
-
-
+const userController = require('../server/controllers/user.controllers');
 
 const resolvers = {
   Query: {
-    users: async () => getUsers(),
+    getUsers: userController.getUsersFromUsersTable,
   },
-  Mutation : {
-    createUser: async(req:any) => {
-      console.log('req -->', req.body)
-      const {username,password,display_name, picture_url} = req.body;
-      const query = {
-        text: 'INSERT INTO users(username,password, display_name,  picture_url) VALUES($1, $2, $3, $4)',
-        values: [username,password, display_name, picture_url],
-      };
-      const response = await pool.query(query,[uuidv4(),username,password, display_name, picture_url]);
-      console.log('response-->', response)
-      return response // 'Successfully created!';
-    }
-  }
+  Mutation: {
+    createUser: userController.createUser,
+  },
 };
-export default resolvers;
+
+export {};
+module.exports = resolvers;
